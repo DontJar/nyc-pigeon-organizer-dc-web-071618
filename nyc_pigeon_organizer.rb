d44b10017a10f304886f9122d41d13afbd91f5af
Jon Schulman <schulman99@gmail.com>
2018-07-10 02:18:26 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,14 +5,25 @@ pigeon_list = {}
   data.each do |col_gen_liv, nxt_data|
     nxt_data.each do |facts, name|
       name.each do |p_name|
-        pigeon_list[p_name] ||= col_gen_liv
         pigeon_list[p_name] ||= {}
-        # binding.pry
-        # pigeon_list[p_name] ||= []
-        # pigeon_list[p_name] << facts.to_s
+        pigeon_list[p_name][col_gen_liv] ||= []
+        pigeon_list[p_name] << facts.to_s
       end
     end
   end
   binding.pry
 pigeon_list
 end
+
+#   data.each do |trait, options|
+#     options.each do |option, pigeons|
+#       pigeons.each do |pigeon|
+#         organized[pigeon] ||= {}
+#         organized[pigeon][trait] ||= []
+#         organized[pigeon][trait] << option.to_s
+#       end
+#     end
+#   end
+
+#   organized
+# end
