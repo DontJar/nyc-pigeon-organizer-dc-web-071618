0c83f352555b78383408ae77f9c005fb591b44ad
Jon Schulman <schulman99@gmail.com>
2018-07-10 01:47:23 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,8 +5,9 @@ pigeon_list = {}
   data.each do |col_gen_liv, nxt_data|
     nxt_data.each do |facts, name|
       name.each do |p_name|
-        pigeon_list[p_name] ||= []
-        pigeon_list[p_name] << facts.to_s
+        pigeon_list[p_name] = col_gen_liv
+        pigeon_list[p_name][col_gen_liv] ||= []
+        pigeon_list[p_name][col_gen_liv] << facts.to_s
       end
     end
   end
