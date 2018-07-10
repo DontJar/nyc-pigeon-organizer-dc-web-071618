a83e9e686f9da76d6ecd35b3e85399c5876d0ca2
Jon Schulman <schulman99@gmail.com>
2018-07-10 02:19:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_list = {}
+  data.each do |col_gen_liv, nxt_data|
+    nxt_data.each do |facts, name|
+      name.each do |p_name|
+        pigeon_list[p_name] ||= {}
+        pigeon_list[p_name][col_gen_liv] ||= []
+        pigeon_list[p_name][col_gen_liv] << facts.to_s
+      end
+    end
+  end
+  # binding.pry
+pigeon_list
 end
