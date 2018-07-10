e3e6faf5610749be5cd23881bf8176bad9612ca3
Jon Schulman <schulman99@gmail.com>
2018-07-10 01:25:59 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,11 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-pigeon_regorg = {}
+pigeon_regorg = Hash.new
   data.map do |col_gen_liv, nxt_data|
-# binding.pry
     nxt_data do |facts, name|
-binding.pry
       pigeon_regorg[facts] = name
-      # binding.pry
     end
-    # binding.pry
   end
-# binding.pry
 pigeon_regorg
 end
