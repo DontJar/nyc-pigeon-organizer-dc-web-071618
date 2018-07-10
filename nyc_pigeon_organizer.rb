db3fee5905bdbdb8fd5ec2abc3ba9a1ac5798171
Jon Schulman <schulman99@gmail.com>
2018-07-10 00:57:40 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+# pigeon_regorg = {}
+
+
+  #   languages.each do |oo_f, language|
+  #     language.each do |name, info|
+  #       new_hash[name] ||= info         #adds "info" - which is already in there as :type => "compiled"  (etc.)
+  #       new_hash[name][:style] ||= []   #creates an empty array as the value of :style (if there's nothing there already)
+  #       new_hash[name][:style] << oo_f  #pushes oo_f into the :style array - whcich takes care of getting both 'oo' and 'functional' into :javascript
+  #     end
+  #   end
+  #   new_hash
+  # end
+
+  pigeon_regorg
 end
